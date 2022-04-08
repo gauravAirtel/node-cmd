67e17db49f852325c3a2bf77f044dcadfdfa2699
Akshay Poduval <akshay.poduval@wynk.in>
2022-04-08 12:04:35 +0530

    adding log path
    adding log path

--- src/helpers/logger/config.js
+++ src/helpers/logger/config.js
@@ -2,6 +2,6 @@ module.exports = {
   ONE_DAY: 86400000,
   FOUR_HOURS: 14400000,
   ONE_WEEK: 604800000,
-  LOG_FILE_PATH: '/data/gaming/gaming-backend',
+  LOG_FILE_PATH: '/data/ffmpegLogs',
   ROTATING_PERIOD_IN_HOURS: 4,
 };
